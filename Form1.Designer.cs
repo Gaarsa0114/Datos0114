f56cd08c6e162e11534d0c710af669920796ce18
gaarsa0114 <santiago.gasca717@pascualbravo.edu.co>
2025-03-06 15:19:26 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace datos1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbluser = new System.Windows.Forms.Label();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.btnok = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluser.Location = new System.Drawing.Point(252, 135);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(36, 16);
+            this.lbluser.TabIndex = 0;
+            this.lbluser.Text = "User";
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpass.Location = new System.Drawing.Point(252, 186);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(67, 16);
+            this.lblpass.TabIndex = 1;
+            this.lblpass.Text = "Password";
+            // 
+            // btnok
+            // 
+            this.btnok.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnok.Location = new System.Drawing.Point(262, 305);
+            this.btnok.Name = "btnok";
+            this.btnok.Size = new System.Drawing.Size(80, 23);
+            this.btnok.TabIndex = 2;
+            this.btnok.Text = "Ok";
+            this.btnok.UseVisualStyleBackColor = true;
+            this.btnok.Click += new System.EventHandler(this.btnok_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncancel.Location = new System.Drawing.Point(442, 305);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(85, 23);
+            this.btncancel.TabIndex = 3;
+            this.btncancel.Text = "Cancel";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(360, 132);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(191, 20);
+            this.txtuser.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(360, 183);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(191, 20);
+            this.txtpass.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnok);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.lbluser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.Button btnok;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.TextBox txtpass;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datos1
+{
+
+    public partial class Form1 : Form
+    {
+        private string usuarioCorrecto = "santiago";
+        private string contraseñaCorrecta = "santiago0114";
+
+        private int intentos = 0;
+        public Form1()
+            
+        {
+            InitializeComponent();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            if
+                 (txtuser.Text == usuarioCorrecto && txtpass.Text == contraseñaCorrecta)
+            {
+                MessageBox.Show($"Bienvenido user", "login exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+   
+                this.Hide();
+
+                this.Hide();
+            }
+
+            else
+            {
+                intentos++;
+                if (intentos >= 3)
+                {
+                    MessageBox.Show("Numero Maximo de Errores", "Intentos Agotados", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o Contraseña Inconrrectos", "Intenta de Nuevo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
